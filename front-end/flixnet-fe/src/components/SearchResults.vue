fc5c13ec99782bb96c284f38fd716d28e6439aa6
Marcelo Vergara <marcelorv@gmail.com>
2022-06-03 18:39:54 -0300

    favourite items adjustments

--- front-end/flixnet-fe/src/components/SearchResults.vue
+++ front-end/flixnet-fe/src/components/SearchResults.vue
@@ -1,23 +1,42 @@
 <template>
   <div style="color: #dadada;" class="mx-auto mb-4 p-2">
-    <span v-if="movieLst[0].Title === 'Not found'">No results found for <strong>{{ movieLst[0].Search }}</strong> </span>
+    <!--  search result-->
+    <span v-if="movieLst[0].Title === 'Not found'">No results found for <strong>{{ movieLst[0].Search }}</strong></span>
     <div v-if="movieLst[0].Title !== 'No value' && movieLst[0].Title !== 'Not found'" class="text-start mt-4">
       <span style="font-size: 28px;">Search Results For <strong>{{ movieLst[0].Search}}</strong></span>
       <hr style="border: 1px solid white">
     </div>
     <div v-if="movieLst[0].Title !== 'No value' && movieLst[0].Title !== 'Not found'" class="d-flex flex-wrap gap-5 flex-grow-1 w-100">
-      <div @mouseover="overedItem($event, title.imdbID)" @mouseleave="overedItem($event, '')" style="position: relative" v-for="(title, idx) in movieLst" :key="idx" v-b-tooltip.hover :title="`${title.Year}, ${title.Title}`">
+      <div v-for="(title, idx) in movieLst" :key="idx"  @mouseover="overedItem($event, title.imdbID)" @mouseleave="overedItem($event, '')" style="position: relative" v-b-tooltip.hover :title="`${title.Year}, ${title.Title}`">
         <div v-b-hover="mainHandleHover">
-          <b-icon v-if="selectedItem === title.imdbID && !mainhoverHeartBool" class="h1" style="position: absolute; left: 10px; top: 10px" icon="heart" variant="danger"></b-icon>
-          <b-icon @click="setItem(title)" v-if="selectedItem === title.imdbID && mainhoverHeartBool" class="h1" style="position: absolute; left: 10px; top: 10px" icon="heart-fill" variant="danger"></b-icon>
+          <b-icon v-if="selectedItem === title.imdbID && !mainhoverHeartBool" class="h1 heartPosition" icon="heart" variant="danger"></b-icon>
+          <b-icon v-if="(selectedItem === title.imdbID && mainhoverHeartBool) || favouritiesLst.filter(f => f.imdbID === title.imdbID).length > 0" @click="setItem(title)"  class="h1 heartPosition" icon="heart-fill" variant="danger"></b-icon>
         </div>
-        <b-img v-if="title.Poster !== 'N/A'" @click="openModal(title.imdbID)"  fluid thumbnail style="height: 390px; width: 260px" :src="title.Poster" :alt="title.Title"></b-img>
-        <div v-else @click="openModal(title.imdbID)"  style="height: 390px; width: 260px; border: 4px solid white" class="p-3 rounded-2">
+        <b-img v-if="title.Poster !== 'N/A'" @click="openModal(title.imdbID)" fluid class="posterSize" :src="title.Poster" :alt="title.Title"></b-img>
+        <div v-else @click="openModal(title.imdbID)" class="p-3 rounded-2 posterSize">
           <span style="color: #b9b9b9">{{ title.Title }}</span>
           <span style="color: #484747; font-size: 200px" class="material-symbols-outlined">theaters</span>
         </div>
       </div>
     </div>
+    <!--    whish/favourited list-->
+    <div v-if="favouritiesLst.length > 0" class="text-start mt-4">
+      <span style="font-size: 28px;">Your Wishlist</span>
+      <hr style="border: 1px solid white">
+    </div>
+    <div v-if="favouritiesLst.length > 0" class="d-flex flex-wrap gap-5 flex-grow-1 w-100" :key="renderAgain">
+      <div v-for="(title, idx) in favouritiesLst" :key="idx" v-b-tooltip.hover :title="`${title.Year}, ${title.Title}`" style="position: relative">
+        <div>
+          <b-icon @click="setItem(title)" v-if="favouritiesLst.filter(f => f.imdbID === title.imdbID).length > 0" class="h1 heartPosition" icon="heart-fill" variant="danger"></b-icon>
+        </div>
+        <b-img v-if="title.Poster !== 'N/A'" @click="openModal(title.imdbID)" fluid class="posterSize" :src="title.Poster" :alt="title.Title"></b-img>
+        <div v-else @click="openModal(title.imdbID)" class="p-3 rounded-2 posterSize">
+          <span style="color: #b9b9b9">{{ title.Title }}</span>
+          <span style="color: #484747; font-size: 200px" class="material-symbols-outlined">theaters</span>
+        </div>
+      </div>
+    </div>
+    <!--    modal-->
     <b-modal style="position: relative" id="item-modal" size="lg" hide-footer header-bg-variant="dark" header-text-variant="light">
       <template #modal-header="{ close }">
         <h5>{{ oneItem.Year }}, {{ oneItem.Title }}</h5>
@@ -26,12 +45,12 @@
         <div v-if="loading" class="text-center p-3">
           <b-spinner variant="secondary" style="width: 6rem; height: 6rem;"></b-spinner>
         </div>
-        <b-card v-else-if="oneItem.Poster !== 'N/A'" body-bg-variant="light" class="mb-3">
+        <b-card :key="renderAgain" v-else-if="oneItem.Poster !== 'N/A'" body-bg-variant="light" class="mb-3">
           <b-row>
             <b-col md="5">
-              <div v-b-hover="handleHover">
-                <b-icon v-if="hoverHeartBool" class="h1" style="position: absolute; left: 5px; top: 5px" icon="heart-fill" variant="danger"></b-icon>
-                <b-icon v-else class="h1" style="position: absolute; left: 5px; top: 5px" icon="heart" variant="danger"></b-icon>
+              <div>
+                <b-icon @click="setItem(oneItem)" class="h1 heartPosition" icon="heart" variant="danger"></b-icon>
+                <b-icon v-if="favouritiesLst.filter(f => f.imdbID === oneItem.imdbID).length > 0" @click="setItem(oneItem)"  class="h1 heartPosition" icon="heart-fill" variant="danger"></b-icon>
               </div>
               <b-card-img :src="oneItem.Poster" :img-alt="oneItem.Title"></b-card-img>
             </b-col>
@@ -39,16 +58,16 @@
               <b-card-text>
                 Plot: {{ oneItem.Plot }}
               </b-card-text>
-              <b-card-text >
+              <b-card-text>
                 Genre: {{ oneItem.Genre }}
               </b-card-text>
-              <b-card-text >
+              <b-card-text>
                 Director: {{ oneItem.Director }}
               </b-card-text>
-              <b-card-text >
+              <b-card-text>
                 Actors: {{ oneItem.Actors }}
               </b-card-text>
-              <b-card-text >
+              <b-card-text>
                 <b-form-rating size="sm" variant="warning" v-model="oneItem.imdbRating" stars="10" readonly show-value inline no-border disabled></b-form-rating>
               </b-card-text>
               <b-card-text v-if="oneItem.Website !== 'N/A' && oneItem.Website !== null">
@@ -98,12 +117,26 @@ export default {
       loading: false,
       mainhoverHeartBool: false,
       hoverHeartBool: false,
-      selectedItem: ''
+      selectedItem: '',
+      get favouritiesLst(){
+        return JSON.parse(localStorage.getItem("favouritesLst"))
+      },
+      renderAgain: false
     }
   },
   methods: {
     setItem(item){
-      console.log(item)
+      let favourities = JSON.parse(localStorage.getItem("favouritesLst")) || []
+      if (favourities.filter(f => f.imdbID === item.imdbID).length === 0){
+        favourities.push(item)
+        localStorage.setItem("favouritesLst", JSON.stringify(favourities))
+        this.renderAgain = !this.renderAgain
+      } else {
+        favourities = favourities.filter((value) => value.imdbID !== item.imdbID)
+        localStorage.setItem("favouritesLst", JSON.stringify(favourities))
+        this.renderAgain = !this.renderAgain
+      }
+      console.log(favourities)
     },
     mainHandleHover(isHovered){
       this.mainhoverHeartBool = isHovered
@@ -119,7 +152,6 @@ export default {
       this.loading = true
       axios.get("http://localhost:8080/api/one?imdbID=" + id)
           .then((res) => {
-            console.log(res)
             this.oneItem = res.data
             this.$bvModal.show('item-modal')
             // loading behaviour - simulating slow networks
@@ -134,10 +166,22 @@ export default {
             this.loading = false
           })
     }
+  },
+  mounted() {
+
   }
 }
 </script>
 
 <style scoped>
-
+.heartPosition{
+  position: absolute;
+  left: 10px;
+  top: 10px
+}
+.posterSize{
+  height: 390px;
+  width: 260px;
+  border: 4px solid white
+}
 </style>
